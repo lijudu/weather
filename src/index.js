2b7161e7fd5e0dd0ff9883df6a433c5a4b1d42f4
Lisa <mkoliju@gmail.com>
2023-01-21 09:51:36 -0800

    Get local date andt time

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import './styles.css'
+import $ from 'jquery'
+import { format, utcToZonedTime } from 'date-fns'
 
 let city = ""
 let lat = ""
 let lon = ""
 
-city = 'Vancouver'
+city = 'vancouver'
 
+// API Fetch/Promise get lat/long of input city, then convert lat/long to weather data 
+// df6a91bfdf395d8590174317cd545762
 fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}}&limit=1&appid=df6a91bfdf395d8590174317cd545762`, {mode: 'cors'})
     .then(function(response) {
         // response
@@ -21,24 +25,44 @@ fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}}&limit=1&appid=df
         fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=df6a91bfdf395d8590174317cd545762`, {mode: 'cors'})
             .then(res => res.json())
             .then(data => {
-                console.log('temperature is ' + Math.round((data.main.temp - 273.15)))
                 console.log(data)
+
+
+                const icon = data.weather[0].icon
+
+                const dateTime = new Date(data.dt * 1000);
+                const toUtc = dateTime.getTime() + dateTime.getTimezoneOffset() * 60000;
+                const currentLocalTime = toUtc + 1000 * data.timezone;
+                const selectedDate = new Date(currentLocalTime);
+               
+                const date = selectedDate.toLocaleString("en-US", {
+                     day: "numeric",
+                     weekday: "long",
+                     month: "long",
+                });
+                const year = selectedDate.toLocaleString("en-GB", {
+                     year: "numeric",
+                });
+                const hour = selectedDate.toLocaleString("en-GB", {
+                     hour: "2-digit",
+                     minute: "2-digit",
+                     hour12: false,
+                });
+            
+
+                $('#icon-summary').attr('src', `http://openweathermap.org/img/wn/${icon}@2x.png`)
+                $('#worded-summary').text(data.weather[0].description)
+                $('#city').text(city)
+                $('#date').text(date + ' ' + year)
+                $('#time').text(hour)
+                $('#current-temp').text(Math.round((data.main.temp - 273.15)) + ' deg C' )
             })
             .catch(e => {
                 console.log(e)
             })
     })
 
-
-// fetch('http://api.openweathermap.org/data/2.5/weather?lat=49.2608724&lon=-123.113952&appid=df6a91bfdf395d8590174317cd545762', {mode: 'cors'})
-//     .then(function(response){
-//         console.log(response.json())
-//     })
-//     .catch(e => {
-//         console.log(e)
-//     })
-    // df6a91bfdf395d8590174317cd545762
+// Right hand side information 
 
 
     
-console.log('hello')
