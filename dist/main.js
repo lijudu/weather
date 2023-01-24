a419e12e465204477f3d729f0f53c0c098b3104e
Lisa <mkoliju@gmail.com>
2023-01-24 10:39:54 -0800

    Wait for city input

--- dist/main.js
+++ dist/main.js
@@ -11596,70 +11596,77 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-
 let city = ""
 let lat = ""
 let lon = ""
 
-city = 'vancouver'
+
+
+
+// input keypress event listener
+jquery__WEBPACK_IMPORTED_MODULE_1___default()('#getCity').on('keydown', function(e) {
+    if(e.key === 'Enter') {
+        city = jquery__WEBPACK_IMPORTED_MODULE_1___default()('#getCity').val()
+        getCityWeather(city)
+    } 
+})
 
 // API Fetch/Promise get lat/long of input city, then convert lat/long to weather data 
 // df6a91bfdf395d8590174317cd545762
-fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}}&limit=1&appid=df6a91bfdf395d8590174317cd545762`, {mode: 'cors'})
-    .then(function(response) {
-        // response
-        return response.json()
-    })
-    .then(function(response){
-        console.log('lat ' + response[0].lat)
-        console.log('lon ' + response[0].lon)
-        return lat=response[0].lat, lon=response[0].lon
+async function getCityWeather(city) {
+const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}}&limit=1&appid=df6a91bfdf395d8590174317cd545762`, {mode: 'cors'})
+        .then(function(response) {
+            // response
+            return response.json()
+        })
+        .then(function(response){
+            console.log('lat ' + response[0].lat)
+            console.log('lon ' + response[0].lon)
+            return lat=response[0].lat, lon=response[0].lon
 
-    })
-    .then(function(response){
-        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=df6a91bfdf395d8590174317cd545762`, {mode: 'cors'})
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
+        })
+        .then(function(response){
+            fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=df6a91bfdf395d8590174317cd545762`, {mode: 'cors'})
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
 
-                // get weather icon
-                const icon = data.weather[0].icon
-                // 1) get local time 2) find local timeoffset 3)obtein UTC time 4) obtain city's offset in hours then   
-                const dateTime = new Date(data.dt * 1000);
-                const toUtc = dateTime.getTime() + dateTime.getTimezoneOffset() * 60000;
-                const currentLocalTime = toUtc + 1000 * data.timezone;
-                const selectedDate = new Date(currentLocalTime);
-               
-                const date = selectedDate.toLocaleString("en-US", {
-                     day: "numeric",
-                     weekday: "long",
-                     month: "long",
-                });
-                const year = selectedDate.toLocaleString("en-GB", {
-                     year: "numeric",
-                });
-                const hour = selectedDate.toLocaleString("en-GB", {
-                     hour: "2-digit",
-                     minute: "2-digit",
-                     hour12: false,
-                });
-            
+                    // right hand side information 
+                    // get weather icon
+                    const icon = data.weather[0].icon
+                    // 1) get local time 2) find local timeoffset 3)obtein UTC time 4) obtain city's offset in hours then   
+                    const dateTime = new Date(data.dt * 1000);
+                    const toUtc = dateTime.getTime() + dateTime.getTimezoneOffset() * 60000;
+                    const currentLocalTime = toUtc + 1000 * data.timezone;
+                    const selectedDate = new Date(currentLocalTime);
+                
+                    const date = selectedDate.toLocaleString("en-US", {
+                        day: "numeric",
+                        weekday: "long",
+                        month: "long",
+                    });
+                    const year = selectedDate.toLocaleString("en-GB", {
+                        year: "numeric",
+                    });
+                    const hour = selectedDate.toLocaleString("en-GB", {
+                        hour: "2-digit",
+                        minute: "2-digit",
+                        hour12: false,
+                    });
+                
 
-                jquery__WEBPACK_IMPORTED_MODULE_1___default()('#icon-summary').attr('src', `http://openweathermap.org/img/wn/${icon}@2x.png`)
-                jquery__WEBPACK_IMPORTED_MODULE_1___default()('#worded-summary').text(data.weather[0].description)
-                jquery__WEBPACK_IMPORTED_MODULE_1___default()('#city').text(city)
-                jquery__WEBPACK_IMPORTED_MODULE_1___default()('#date').text(date + ' ' + year)
-                jquery__WEBPACK_IMPORTED_MODULE_1___default()('#time').text(hour)
-                jquery__WEBPACK_IMPORTED_MODULE_1___default()('#current-temp').text(Math.round((data.main.temp)) + ' deg C' )
-            })
-            .catch(e => {
-                console.log(e)
-            })
-    })
-
-// Right hand side information 
-
-
+                    jquery__WEBPACK_IMPORTED_MODULE_1___default()('#icon-summary').attr('src', `http://openweathermap.org/img/wn/${icon}@2x.png`)
+                    jquery__WEBPACK_IMPORTED_MODULE_1___default()('#worded-summary').text(data.weather[0].description)
+                    jquery__WEBPACK_IMPORTED_MODULE_1___default()('#city').text(city)
+                    jquery__WEBPACK_IMPORTED_MODULE_1___default()('#date').text(date + ' ' + year)
+                    jquery__WEBPACK_IMPORTED_MODULE_1___default()('#time').text(hour)
+                    jquery__WEBPACK_IMPORTED_MODULE_1___default()('#current-temp').text(Math.round((data.main.temp)) + ' deg C' )
+                })
+                .catch(e => {
+                    console.log(e)
+                })
+        })
+    }
     
 
 })();
--- src/index.js
+++ src/index.js
@@ -1,68 +1,75 @@
 import './styles.css'
 import $ from 'jquery'
-import { format, utcToZonedTime } from 'date-fns'
 
 let city = ""
 let lat = ""
 let lon = ""
 
-city = 'vancouver'
+
+
+
+// input keypress event listener
+$('#getCity').on('keydown', function(e) {
+    if(e.key === 'Enter') {
+        city = $('#getCity').val()
+        getCityWeather(city)
+    } 
+})
 
 // API Fetch/Promise get lat/long of input city, then convert lat/long to weather data 
 // df6a91bfdf395d8590174317cd545762
-fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}}&limit=1&appid=df6a91bfdf395d8590174317cd545762`, {mode: 'cors'})
-    .then(function(response) {
-        // response
-        return response.json()
-    })
-    .then(function(response){
-        console.log('lat ' + response[0].lat)
-        console.log('lon ' + response[0].lon)
-        return lat=response[0].lat, lon=response[0].lon
+async function getCityWeather(city) {
+const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}}&limit=1&appid=df6a91bfdf395d8590174317cd545762`, {mode: 'cors'})
+        .then(function(response) {
+            // response
+            return response.json()
+        })
+        .then(function(response){
+            console.log('lat ' + response[0].lat)
+            console.log('lon ' + response[0].lon)
+            return lat=response[0].lat, lon=response[0].lon
 
-    })
-    .then(function(response){
-        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=df6a91bfdf395d8590174317cd545762`, {mode: 'cors'})
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
+        })
+        .then(function(response){
+            fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=df6a91bfdf395d8590174317cd545762`, {mode: 'cors'})
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
 
-                // get weather icon
-                const icon = data.weather[0].icon
-                // 1) get local time 2) find local timeoffset 3)obtein UTC time 4) obtain city's offset in hours then   
-                const dateTime = new Date(data.dt * 1000);
-                const toUtc = dateTime.getTime() + dateTime.getTimezoneOffset() * 60000;
-                const currentLocalTime = toUtc + 1000 * data.timezone;
-                const selectedDate = new Date(currentLocalTime);
-               
-                const date = selectedDate.toLocaleString("en-US", {
-                     day: "numeric",
-                     weekday: "long",
-                     month: "long",
-                });
-                const year = selectedDate.toLocaleString("en-GB", {
-                     year: "numeric",
-                });
-                const hour = selectedDate.toLocaleString("en-GB", {
-                     hour: "2-digit",
-                     minute: "2-digit",
-                     hour12: false,
-                });
-            
+                    // right hand side information 
+                    // get weather icon
+                    const icon = data.weather[0].icon
+                    // 1) get local time 2) find local timeoffset 3)obtein UTC time 4) obtain city's offset in hours then   
+                    const dateTime = new Date(data.dt * 1000);
+                    const toUtc = dateTime.getTime() + dateTime.getTimezoneOffset() * 60000;
+                    const currentLocalTime = toUtc + 1000 * data.timezone;
+                    const selectedDate = new Date(currentLocalTime);
+                
+                    const date = selectedDate.toLocaleString("en-US", {
+                        day: "numeric",
+                        weekday: "long",
+                        month: "long",
+                    });
+                    const year = selectedDate.toLocaleString("en-GB", {
+                        year: "numeric",
+                    });
+                    const hour = selectedDate.toLocaleString("en-GB", {
+                        hour: "2-digit",
+                        minute: "2-digit",
+                        hour12: false,
+                    });
+                
 
-                $('#icon-summary').attr('src', `http://openweathermap.org/img/wn/${icon}@2x.png`)
-                $('#worded-summary').text(data.weather[0].description)
-                $('#city').text(city)
-                $('#date').text(date + ' ' + year)
-                $('#time').text(hour)
-                $('#current-temp').text(Math.round((data.main.temp)) + ' deg C' )
-            })
-            .catch(e => {
-                console.log(e)
-            })
-    })
-
-// Right hand side information 
-
-
+                    $('#icon-summary').attr('src', `http://openweathermap.org/img/wn/${icon}@2x.png`)
+                    $('#worded-summary').text(data.weather[0].description)
+                    $('#city').text(city)
+                    $('#date').text(date + ' ' + year)
+                    $('#time').text(hour)
+                    $('#current-temp').text(Math.round((data.main.temp)) + ' deg C' )
+                })
+                .catch(e => {
+                    console.log(e)
+                })
+        })
+    }
     
